0d4413f139fcd68cac0961b9451461c13019a713
Eva Matova <eva.matova6@gmail.com>
2025-08-09 19:20:33 +0300

    feat: enhance sign-in and sign-up pages with navigation links
    - Added navigation links to the sign-in and sign-up pages for improved user experience.
    - Wrapped forms in a section with a header to better structure the layout.

--- src/app/(front-end)/(auth)/sign-in/page.tsx
+++ src/app/(front-end)/(auth)/sign-in/page.tsx
@@ -1,9 +1,16 @@
 import { SignInForm } from '@/auth/nextjs/components/signin-form';
+import Link from 'next/link';
 
 export default function SignInPage() {
   return (
     <div className='flex h-screen items-center justify-center'>
-      <SignInForm />
+      <section>
+        <div className='flex gap-4 pb-4'>
+          <p className='text-muted-foreground'>Sign In</p>
+          <Link href='/sign-up'>Sign Up</Link>
+        </div>
+        <SignInForm />
+      </section>
     </div>
   );
 }
--- src/app/(front-end)/(auth)/sign-up/page.tsx
+++ src/app/(front-end)/(auth)/sign-up/page.tsx
@@ -1,9 +1,16 @@
 import { SignUpForm } from '@/auth/nextjs/components/signup-form';
+import Link from 'next/link';
 
 export default function SignUpPage() {
   return (
     <div className='flex h-screen items-center justify-center'>
-      <SignUpForm />
+      <section>
+        <div className='flex gap-4 pb-4'>
+          <Link href='/sign-in'>Sign In</Link>
+          <p className='text-muted-foreground'>Sign Up</p>
+        </div>
+        <SignUpForm />
+      </section>{' '}
     </div>
   );
 }
