517701ad59fa069c41e5abfb1feac90f264621da
Eva Matova <eva.matova6@gmail.com>
2025-08-11 16:29:05 +0300

    refactor: update sign-in and sign-up page layouts for improved responsiveness
    - Changed the height class from 'h-screen' to 'h-full' in both SignInPage and SignUpPage components to enhance layout responsiveness across different screen sizes.

--- src/app/(front-end)/(auth)/sign-up/page.tsx
+++ src/app/(front-end)/(auth)/sign-up/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function SignUpPage() {
   return (
-    <section className='flex h-screen w-full items-center justify-center'>
+    <section className='flex h-full w-full items-center justify-center'>
       <div className='w-full max-w-sm px-4'>
         <h1 className='mb-8 text-center text-2xl font-bold'>Sign Up</h1>
         <p className='text-muted-foreground mb-8 text-center text-sm'>
